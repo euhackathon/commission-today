b2fca73455a509aa4b2cc035f7de161209cdf8fe
Ans <github@anselmbradford.com>
2014-12-02 20:39:59 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	bootstrap.sh
    	www/app/assets/javascripts/application.js
    	www/config/application.rb
    	www/config/requirejs.yml

--- www/app/assets/javascripts/application.js
+++ www/app/assets/javascripts/application.js
@@ -4,8 +4,37 @@
 // app/assets/javascripts/routes/.
 //
 require([
+  'util/api/Client',
+  'mustache',
   'domReady!'
 ],
-function () {
+function (Client, Mustache) {
   'use strict';
+  var _statusHtml = document.querySelector('.loading-status');
+  var _view;
+  var client = Client.create('http://palcu.ro:8000/api/v1/');
+  client.get('meeting');
+  client.addEventListener('dataloaded', _initialDataLoaded);
+
+  function _initialDataLoaded(evt) {
+    _view = {
+      meeting : client.response.meeting.objects
+    }
+
+    _renderTemplate();
+  }
+
+  function _renderTemplate() {
+    var template = document.querySelector('.content');
+
+    // Render mustache template and apply to page.
+    var rendered = Mustache.render(template.innerHTML, _view);
+    template.innerHTML = rendered;
+
+    // Fade-out loading screen when everything loads.
+    $(_statusHtml).fadeOut('slow', function() {
+      template.classList.remove('hide');
+    });
+  }
+
 });
---
+++ www/app/assets/javascripts/util/api/Client.js
@@ -0,0 +1,143 @@
+define([
+  'jquery'
+],
+function ($) {
+  'use strict';
+
+  var defaultBaseURL = "/api/v1";
+
+  function create(baseURL) {
+    return new Client(baseURL || defaultBaseURL);
+  }
+
+  function Client(baseURL){
+    var _instance = this;
+    var _baseURL = baseURL;
+
+    // The events the API Client broadcasts.
+    var _events = {};
+
+    // The data returned from the API.
+    var response = {};
+
+    // Make a HTTP GET request.
+    // @param url [String] The path, relative to the API's base URL.
+    // @param options [Object] An optional hash of settings.
+    //   May include 'callback'—a callback function, and 'page'—the
+    //   page to query.
+    function get(url, options) {
+      options = options || {};
+
+      var params = '';
+      if (options.page) {
+         var delimiter = '?';
+        if (url.indexOf('?') != -1)
+          delimiter = '&';
+        params = delimiter + 'page=' + options.page;
+      }
+      _dispatchEvent('dataloading', {'path':url});
+      $.getJSON(_baseURL + url + params + '?format=json', function(data, status, xhr) {
+        if (status === 'success')
+          _dataLoaded(data, url, options.page, options.callback);
+        // TODO: Add handling for different server responses:
+        // 'notmodified', 'error', 'timeout', and 'parsererror'.
+      });
+    }
+
+    // @param data [Object] The data payload of the response to a get request.
+    // @param url [String] The URL passed to a get request for this response.
+    // @param callback [Function] An optional callback function to execute when
+    //   the request is complete.
+    function _dataLoaded(data, url, page, callback) {
+      // Build path on client and attach data to last node.
+      _buildPathFrom(data, url);
+
+      _dispatchEvent('dataloaded', {path:url, data:data, page:page});
+      if (callback)
+        callback.call(null, {path:url, data:data, page:page});
+    }
+
+    // @param data [Object] The data payload of the response to a get request.
+    // @param url [String] A URL path, with a preceding slash.
+    //   ex. /organizations/3881
+    function _buildPathFrom(data, url) {
+      // Strip parameters from url if present.
+      if (url.indexOf('?') != -1)
+          url = url.substring(0, url.indexOf('?'));
+
+      // Break URL path into pieces so it can be used in an iteration loop.
+      var tokens = url.split('/');
+
+      // Iterate through path and attach structure to response if needed.
+      var numTokens = tokens.length;
+      var token;
+      var count = 1;
+      var thePath = response;
+      var lastPath = thePath;
+
+      token = tokens[0];
+      if (!lastPath[token])
+        lastPath[token] = {};
+      if (count !== numTokens)
+        lastPath = lastPath[token];
+      while (count+1 < numTokens) {
+        token = tokens[count++];
+        if (!lastPath[token])
+          lastPath[token] = {};
+        if (count === numTokens) break;
+        lastPath = lastPath[token];
+      }
+      var lastNode = lastPath[token];
+
+      // Loop through data and populate last node of path with data properties.
+      for (var d in data) {
+        if (data.hasOwnProperty(d))
+          lastNode[d] = data[d];
+      }
+    }
+
+    // @param event [String] The event name to listen for.
+    // Supports 'dataloading' and 'dataloaded'.
+    // @param callback [Function] The function called when the event has fired.
+    function addEventListener(name, callback) {
+      if (_events.hasOwnProperty(name))
+        _events[name].push(callback);
+      else
+        _events[name] = [callback];
+    }
+
+    // @param event [String] The event name to listen for.
+    // Supports 'dataloading' and 'dataloaded'.
+    // @param callback [Function] The function called when the event has fired.
+    function removeEventListener(name, callback) {
+      if (!_events.hasOwnProperty(name))
+        return;
+
+      var index = _events[name].indexOf(callback);
+      if (index != -1)
+        _events[name].splice(index, 1);
+    }
+
+    // @param name [String] The type of event to broadcast.
+    // Supports 'dataloading' and 'dataloaded'.
+    function _dispatchEvent(name, options) {
+      if (!_events.hasOwnProperty(name)) return;
+      options = options || {};
+
+      var evts = _events[name], len = evts.length;
+      for (var i = 0; i < len; i++) {
+        evts[i].call(null, options);
+      }
+    }
+
+    _instance.get = get;
+    _instance.addEventListener = addEventListener;
+    _instance.removeEventListener = removeEventListener;
+    _instance.response = response;
+    return _instance;
+  }
+
+  return {
+    create:create
+  }
+});
